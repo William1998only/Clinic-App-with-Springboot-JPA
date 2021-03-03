6205496eeca8014cbb13d067196299829036259c
William1998only <47975332+William1998only@users.noreply.github.com>
2021-03-03 15:18:26 +0700

    Add files via upload

---
+++ KlinikApplication.java
@@ -0,0 +1,12 @@
+package com.lawencon.klinik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlinikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlinikApplication.class, args);
+	}
+}
