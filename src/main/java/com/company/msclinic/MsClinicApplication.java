38f1f4c55ead8b685326e87da90e275adb864d59
William <Williamgolden54@gmail.com>
2022-12-23 13:05:24 +0700

    Initialize project

---
+++ src/main/java/com/company/msclinic/MsClinicApplication.java
@@ -0,0 +1,13 @@
+package com.company.msclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/company/msclinic/MsClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lawencon.klinik.src.test.java.com.company.msclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
