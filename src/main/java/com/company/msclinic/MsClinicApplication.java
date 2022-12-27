9af580f77b54067e519ff168cf36295d0103a4fc
William <Williamgolden54@gmail.com>
2022-12-27 15:40:52 +0700

    Add swagger information

--- src/main/java/com/company/msclinic/MsClinicApplication.java
+++ src/main/java/com/company/msclinic/MsClinicApplication.java
@@ -1,9 +1,12 @@
 package com.company.msclinic;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Clinic Apllication", version = "0.0.1-SNAPSHOT"))
 public class MsClinicApplication {
 
     public static void main(String[] args) {
