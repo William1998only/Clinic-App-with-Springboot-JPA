edd34a5b4de604761a85b9660c1f4c944e77b0d8
William <Williamgolden54@gmail.com>
2022-12-28 16:19:43 +0700

    Validate request dto

--- src/main/java/com/company/msclinic/controller/MedicineTypeController.java
+++ src/main/java/com/company/msclinic/controller/MedicineTypeController.java
@@ -2,6 +2,7 @@ package com.company.msclinic.controller;
 
 import com.company.msclinic.dto.InsertMedicineTypeRequestDTO;
 import com.company.msclinic.service.MedicineTypeService;
+import com.company.msclinic.service.ValidationService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -16,21 +17,19 @@ import static com.company.msclinic.constant.Url.MEDICINE_TYPE;
 @RequestMapping(value = MEDICINE_TYPE, produces = MediaType.APPLICATION_JSON_VALUE)
 public class MedicineTypeController {
   private final MedicineTypeService medicineTypeService;
+  private final ValidationService validationService;
 
-  public MedicineTypeController(MedicineTypeService medicineTypeService) {
+  public MedicineTypeController(MedicineTypeService medicineTypeService, ValidationService validationService) {
     this.medicineTypeService = medicineTypeService;
+    this.validationService = validationService;
   }
 
   @PostMapping
   public ResponseEntity<String> insertMedicineType(@RequestBody InsertMedicineTypeRequestDTO request) {
-    try {
-      medicineTypeService.insertMedicineType(request);
+    validationService.validate(request);
 
-      return new ResponseEntity<>("Input success", HttpStatus.OK);
-    } catch (Exception exception) {
-      exception.printStackTrace();
+    medicineTypeService.insertMedicineType(request);
 
-      return new ResponseEntity<>("Input fail", HttpStatus.INTERNAL_SERVER_ERROR);
-    }
+    return new ResponseEntity<>("Input success", HttpStatus.OK);
   }
 }
--- src/main/java/com/company/msclinic/dto/InsertMedicineTypeRequestDTO.java
+++ src/main/java/com/company/msclinic/dto/InsertMedicineTypeRequestDTO.java
@@ -2,8 +2,16 @@ package com.company.msclinic.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Data
 public class InsertMedicineTypeRequestDTO {
+  @NotBlank
+  @Size(max = 20)
   private String medicineType;
+
+  @NotNull
   private Integer medicineCodeType;
 }
