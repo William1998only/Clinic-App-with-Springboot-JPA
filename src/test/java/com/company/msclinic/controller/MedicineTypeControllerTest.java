c0de79543f5e62c6daaa0f1d26c2d71b1ecde637
William <Williamgolden54@gmail.com>
2022-12-28 16:19:56 +0700

    Add negative test case

--- src/test/java/com/company/msclinic/controller/MedicineTypeControllerTest.java
+++ src/test/java/com/company/msclinic/controller/MedicineTypeControllerTest.java
@@ -4,14 +4,19 @@ import com.company.msclinic.constant.Url;
 import com.company.msclinic.controller.abstraction.AbstractControllerTest;
 import com.company.msclinic.dto.InsertMedicineTypeRequestDTO;
 import com.company.msclinic.service.MedicineTypeService;
+import com.company.msclinic.service.ValidationService;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MvcResult;
+
+import javax.validation.ConstraintViolationException;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -26,6 +31,9 @@ class MedicineTypeControllerTest extends AbstractControllerTest {
   @MockBean
   private MedicineTypeService medicineTypeService;
 
+  @MockBean
+  private ValidationService validationService;
+
   @Nested
   @DisplayName("Insert Medicine Type Test Case")
   class InsertMedicineTypeTestCase {
@@ -36,6 +44,7 @@ class MedicineTypeControllerTest extends AbstractControllerTest {
       InsertMedicineTypeRequestDTO request = new InsertMedicineTypeRequestDTO();
       request.setMedicineType("sirup");
       request.setMedicineCodeType(1);
+      doNothing().when(validationService).validate(request);
       doNothing().when(medicineTypeService).insertMedicineType(request);
 
       MvcResult mvcResult = mockMvc.perform(
@@ -55,16 +64,72 @@ class MedicineTypeControllerTest extends AbstractControllerTest {
       InsertMedicineTypeRequestDTO request = new InsertMedicineTypeRequestDTO();
       request.setMedicineType("sirup");
       request.setMedicineCodeType(1);
+      doNothing().when(validationService).validate(request);
       doThrow(new RuntimeException()).when(medicineTypeService).insertMedicineType(request);
+
+      mockMvc.perform(
+          post(Url.MEDICINE_TYPE)
+            .content(objectMapper.writeValueAsString(request))
+            .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isInternalServerError());
+    }
+
+    @SneakyThrows
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("Expect to return status 400 when medicineType is null or empty")
+    void fail_medicine_type_null_or_empty(String medicineType) {
+      InsertMedicineTypeRequestDTO request = new InsertMedicineTypeRequestDTO();
+      request.setMedicineType(medicineType);
+      request.setMedicineCodeType(1);
+      doThrow(new ConstraintViolationException("medicineType: must not be blank", null)).when(validationService).validate(request);
 
       MvcResult mvcResult = mockMvc.perform(
           post(Url.MEDICINE_TYPE)
             .content(objectMapper.writeValueAsString(request))
             .contentType(MediaType.APPLICATION_JSON))
-        .andExpect(status().isInternalServerError())
+        .andExpect(status().isBadRequest())
         .andReturn();
 
-      assertThat(mvcResult.getResponse().getContentAsString(), is("Input fail"));
+      assertThat(mvcResult.getResponse().getContentAsString(), is("medicineType: must not be blank"));
+    }
+
+    @Test
+    @SneakyThrows
+    @DisplayName("Expect to return status 400 when medicineCodeType is null")
+    void fail_medicine_code_type_null() {
+      InsertMedicineTypeRequestDTO request = new InsertMedicineTypeRequestDTO();
+      request.setMedicineType("cyrup");
+      request.setMedicineCodeType(null);
+      doThrow(new ConstraintViolationException("medicineCodeType: must not be null", null)).when(validationService).validate(request);
+
+      MvcResult mvcResult = mockMvc.perform(
+          post(Url.MEDICINE_TYPE)
+            .content(objectMapper.writeValueAsString(request))
+            .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isBadRequest())
+        .andReturn();
+
+      assertThat(mvcResult.getResponse().getContentAsString(), is("medicineCodeType: must not be null"));
+    }
+
+    @Test
+    @SneakyThrows
+    @DisplayName("Expect to return status 400 when medicineType size is more than 20")
+    void fail_medicine_type_size_more_than_20() {
+      InsertMedicineTypeRequestDTO request = new InsertMedicineTypeRequestDTO();
+      request.setMedicineType("ksoakosakoskaoksoakoaskosakokasoksaokak");
+      request.setMedicineCodeType(null);
+      doThrow(new ConstraintViolationException("medicineType: size must be between 0 and 20", null)).when(validationService).validate(request);
+
+      MvcResult mvcResult = mockMvc.perform(
+          post(Url.MEDICINE_TYPE)
+            .content(objectMapper.writeValueAsString(request))
+            .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isBadRequest())
+        .andReturn();
+
+      assertThat(mvcResult.getResponse().getContentAsString(), is("medicineType: size must be between 0 and 20"));
     }
   }
 }
